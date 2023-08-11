27725b18614a5217b0e0ba8edf6cdb3bc6631b08
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-08-11 12:24:07 +0200

    [PAGOPA-1143] feat: add JUnit test for /authorization/* APIs

---
+++ src/test/java/it/gov/pagopa/authorizer/config/controller/AuthorizationControllerTest.java
@@ -0,0 +1,222 @@
+package it.gov.pagopa.authorizer.config.controller;
+
+import it.gov.pagopa.authorizer.config.Application;
+import it.gov.pagopa.authorizer.config.exception.AppError;
+import it.gov.pagopa.authorizer.config.exception.AppException;
+import it.gov.pagopa.authorizer.config.model.PageInfo;
+import it.gov.pagopa.authorizer.config.model.authorization.*;
+import it.gov.pagopa.authorizer.config.service.AuthorizationService;
+import it.gov.pagopa.authorizer.config.util.CommonUtil;
+import it.gov.pagopa.authorizer.config.util.MockBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+class AuthorizationControllerTest {
+
+  @Autowired private MockMvc mvc;
+
+  @MockBean private AuthorizationService authorizationService;
+
+  @ParameterizedTest
+  @CsvSource({
+          "fakedomain,",
+          "fakedomain,77777777777",
+  })
+  void getAuthorizations_200(String domain, String ownerId) throws Exception {
+    String url = String.format("/authorizations?page=0&limit=50&domain=%s&ownerId=%s", domain, ownerId);
+    // mocking invocation
+    when(authorizationService.getAuthorizations(anyString(), anyString(), any(Pageable.class)))
+            .thenReturn(MockBuilder.getAuthorizations(domain, ownerId));
+    // executing API call
+    mvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void getAuthorization_200() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    when(authorizationService.getAuthorization(anyString()))
+            .thenReturn(MockBuilder.getAuthorization(0, id, "some-domain", "some-owner"));
+    // executing API call
+    mvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void getAuthorization_404() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    when(authorizationService.getAuthorization(anyString())).thenThrow(new AppException(AppError.NOT_FOUND_NO_VALID_AUTHORIZATION, id));
+    // executing API call
+    mvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void createAuthorization_200() throws Exception {
+    String id = "some-uuid";
+    String url = "/authorizations/";
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.createAuthorization(any(Authorization.class)))
+            .thenReturn(mockedResource);
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(post(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void createAuthorization_400() throws Exception {
+    String id = "some-uuid";
+    String url = "/authorizations/";
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.createAuthorization(any(Authorization.class))).thenThrow(new AppException(HttpStatus.BAD_REQUEST, "Bad Request", "Some validation error"));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(post(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void createAuthorization_409() throws Exception {
+    String id = "some-uuid";
+    String url = "/authorizations/";
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.createAuthorization(any(Authorization.class))).thenThrow(new AppException(AppError.CONFLICT_AUTHORIZATION_ALREADY_EXISTENT, "", ""));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(post(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void createAuthorization_500() throws Exception {
+    String id = "some-uuid";
+    String url = "/authorizations/";
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.createAuthorization(any(Authorization.class))).thenThrow(new AppException(AppError.INTERNAL_SERVER_ERROR, "", ""));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(post(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void updateAuthorization_200() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.updateAuthorization(anyString(), any(Authorization.class)))
+            .thenReturn(mockedResource);
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(put(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void updateAuthorization_400() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.updateAuthorization(anyString(), any(Authorization.class))).thenThrow(new AppException(HttpStatus.BAD_REQUEST, "Bad Request", "Some validation error"));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(put(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void updateAuthorization_404() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.updateAuthorization(anyString(), any(Authorization.class))).thenThrow(new AppException(AppError.NOT_FOUND_NO_VALID_AUTHORIZATION, id));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(put(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void updateAuthorization_500() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    Authorization mockedResource = MockBuilder.getAuthorization(0, id, "some-domain", "some-owner");
+    when(authorizationService.updateAuthorization(anyString(), any(Authorization.class))).thenThrow(new AppException(AppError.INTERNAL_SERVER_ERROR, "", ""));
+    // executing API call
+    String request = MockBuilder.toJson(mockedResource);
+    mvc.perform(put(url).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  @Test
+  void deleteAuthorization_204() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    doNothing().when(authorizationService).deleteAuthorization(anyString());
+    // executing API call
+    mvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+  }
+
+  @Test
+  void deleteAuthorization_404() throws Exception {
+    String id = "some-uuid";
+    String url = String.format("/authorizations/%s", id);
+    // mocking invocation
+    doThrow(new AppException(AppError.NOT_FOUND_NO_VALID_AUTHORIZATION, id)).when(authorizationService).deleteAuthorization(anyString());
+    // executing API call
+    mvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+}
---
+++ src/test/java/it/gov/pagopa/authorizer/config/util/MockBuilder.java
@@ -0,0 +1,86 @@
+package it.gov.pagopa.authorizer.config.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.gov.pagopa.authorizer.config.model.PageInfo;
+import it.gov.pagopa.authorizer.config.model.authorization.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MockBuilder {
+
+    private MockBuilder() {}
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(object);
+    }
+
+    public static AuthorizationList getAuthorizations(String domain, String ownerId) {
+        return AuthorizationList.builder()
+                .authorizations(IntStream.rangeClosed(1, 5)
+                        .mapToObj(order -> getAuthorization(order, "uuid-" + order, domain, ownerId))
+                        .collect(Collectors.toList()))
+                .pageInfo(PageInfo.builder()
+                        .page(0)
+                        .limit(50)
+                        .totalPages(1)
+                        .itemsFound(5)
+                        .build())
+                .build();
+    }
+
+    public static Authorization getAuthorization(int order, String id, String domain, String ownerId) {
+        return Authorization.builder()
+                .id(id)
+                .domain(domain)
+                .subscriptionKey("subkey-" + order)
+                .description("Authorization number " + order)
+                .owner(AuthorizationOwner.builder()
+                        .id(Optional.ofNullable(ownerId).orElse("default-owner-id"))
+                        .name("Owner-" + Optional.ofNullable(ownerId).orElse("default-owner-id"))
+                        .type(OwnerType.CI)
+                        .build())
+                .authorizedEntities(List.of(
+                        AuthorizationEntity.builder()
+                                .name("entity-name-" + (order * 2))
+                                .value("entity-" + (order * 2))
+                                .build(),
+                        AuthorizationEntity.builder()
+                                .name("entity-name-" + (order * 2))
+                                .values(List.of(
+                                        "sub-entity-A" + (order * 2),
+                                        "sub-entity-B" + (order * 2)))
+                                .build()))
+                .otherMetadata(List.of(
+                        AuthorizationMetadata.builder()
+                                .name("Appearance")
+                                .shortKey("_app")
+                                .content(List.of(
+                                        AuthorizationGenericKeyValue.builder()
+                                                .key("color")
+                                                .value("green")
+                                                .build(),
+                                        AuthorizationGenericKeyValue.builder()
+                                                .key("font")
+                                                .value(String.valueOf(order))
+                                                .build()))
+                                .build(),
+                        AuthorizationMetadata.builder()
+                                .name("Language keywords")
+                                .shortKey("_kwd")
+                                .content(List.of(
+                                        AuthorizationGenericKeyValue.builder()
+                                                .key("some-new-futuristic-language")
+                                                .values(List.of("MOVE" + order, "DIV" + order, "->"))
+                                                .build()))
+                                .build()
+                ))
+                .insertedAt("2023-01-01 12:00:00")
+                .lastUpdate("2023-06-01 20:00:00")
+                .lastForcedRefresh("2023-12-01 08:00:00")
+                .build();
+    }
+}
